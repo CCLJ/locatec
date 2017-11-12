9954f09cc8c38a80589908d56218478f32b98213
Jose Carvajal <josecarcar1@gmail.com>
2017-11-11 18:39:27 -0600

    Fixed spacing for cards

--- js/loadLists.js
+++ js/loadLists.js
@@ -16,13 +16,13 @@ $(document).ready( function(){
       var newHtml = "";
       for(var i = 0; i < dataJson.length; i++) {
         newHtml += "<div class='card'> <img class='card-img-top' src='...' alt='Card image cap'>";
-        newHtml += '<div class="card-body">';
+        newHtml += '<div class="card-block">';
         newHtml += '<h4 class="card-title">' + dataJson[i].name + '</h4>';
         newHtml += '<p class="card-text">' + dataJson[i].description + '</p>';
         newHtml += "<p class='card-text'>Found by: " + dataJson[i].found_by + "</p>";
         newHtml += '<p class="card-text"><small class="text-muted">Date found: ' + dataJson[i].date_found + '</small></p>';
-        newHtml += '<div class="card-footer"><button class="btn btn-dos" type="button" name="button">Claim</button></div>'
-        newHtml += "</div></div>";
+        newHtml += '</div><div class="card-footer"><button class="btn btn-dos" type="button" name="button">Claim</button></div>'
+        newHtml += "</div>";
       }
       $("#objectsList").append(newHtml);
     },
