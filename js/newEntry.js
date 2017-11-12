5104a4ed7c6a7fe611686f9d41267a4753ab98d2
Jose Carvajal <josecarcar1@gmail.com>
2017-11-11 23:33:15 -0600

    Added new entry page

---
+++ js/newEntry.js
@@ -0,0 +1,58 @@
+$(document).ready( function(){
+
+  $("#newEntry").on("click",
+    function() {
+      // ---------------------------------------------------------------------
+      var objName = $("#nameInput").val();
+      var objDescription = $("#descInput").val();
+      var foundBy = $("#foundBy").val();
+
+      if(objName != "" && objDescription != "" && foundBy != "") {
+        // names in the service - php file
+        $("#pwdloginError").text("");
+        $("#userloginError").text("");
+        $("#foundError").text("");
+
+        var jsonToSend = {
+          // uName is the email
+          "uName": objName,
+          "uDescription": objDescription,
+          "uFoundBy": foundBy,
+          "action" : "NEW-OBJECT"
+        };
+
+        $.ajax({
+          url: "./php/applicationLayer.php",
+          type: "POST",
+          data: jsonToSend,
+          ContentType: "application/json",
+          dataType: "json",
+          success: function(dataJson) {
+            // firstname and lastname are the specified keys on the array in php
+            alert("Object succesfully posted");
+            document.location.href = "home.html";
+          },
+          error: function(erroMsg) {
+            alert(erroMsg.statusText);
+          }
+        });
+      } else {
+        if(objName == "") {
+          $("#userloginError").text("Missing username");
+        } else {
+          $("#userloginError").text("");
+        }
+        if(objDescription == "") {
+          $("#pwdloginError").text("Missing password");
+        } else {
+          $("#pwdloginError").text("");
+        }
+        if(foundBy == "") {
+          $("#foundError").text("Missing ID");
+        } else {
+          $("#foundError").text("");
+        }
+      }
+
+    });
+});
