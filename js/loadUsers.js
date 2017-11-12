7cdcf5d7b064270dc0e013856691610dfc37fea2
Jose Carvajal <josecarcar1@gmail.com>
2017-11-11 19:37:03 -0600

    Front-end for list of users

--- js/loadUsers.js
+++ js/loadUsers.js
@@ -1,6 +1,5 @@
 $(document).ready( function(){
 
-  // load cookie info into text fields
   var jsonToSend = {
     "action" : "LOAD-USERS"
   };
@@ -11,9 +10,21 @@ $(document).ready( function(){
     ContentType: "application/json",
     dataType: "json",
     success: function(dataJson) {
-      console.log(dataJson);
-      // $(".card-deck").addClass("d-flex flex-wrap");
       var newHtml = "";
+      newHtml += '<thead> <tr> <th>ID</th> <th>First Name</th> <th>Last Name</th> <th>Email</th> <th>Select user</th> </tr> </thead>';
+      newHtml += "<tbody>";
+      for (var i = 0; i < dataJson.length; i++) {
+        newHtml += "<tr>";
+        newHtml += "<td>" + dataJson[i].institution_id + "</th>"
+        newHtml += "<td>" + dataJson[i].fName + "</th>"
+        newHtml += "<td>" + dataJson[i].lName + "</th>"
+        newHtml += "<td>" + dataJson[i].email + "</th>"
+        newHtml += "<td> <button class='btn btn-primary selectUser' type='button' name='button'>See profile</button></th>"
+        newHtml += "</tr>"
+      }
+      newHtml += "</tbody>";
+      console.log(newHtml);
+      $("#objectsList").append(newHtml);
     },
     error: function(erroMsg) {
       alert(erroMsg.statusText);
