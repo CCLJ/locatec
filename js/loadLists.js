f7f91d8183925256caf4a7f62b11d621308a289e
Jose Carvajal <josecarcar1@gmail.com>
2017-11-11 23:08:37 -0600

    Clear results

--- js/loadLists.js
+++ js/loadLists.js
@@ -30,5 +30,37 @@ $(document).ready( function(){
     }
   });
 
+  $("#clearBtn").on("click", function() {
+    var jsonToSend = {
+      "action" : "LOAD-OBJECTS"
+    };
+    $.ajax({
+      url: "./php/applicationLayer.php",
+      type: "POST",
+      data: jsonToSend,
+      ContentType: "application/json",
+      dataType: "json",
+      success: function(dataJson) {
+        // $(".card-deck").addClass("d-flex flex-wrap");
+        $("#objectsList").text("");
+        var newHtml = "";
+        for(var i = 0; i < dataJson.length; i++) {
+          newHtml += "<div class='card'> <img class='card-img-top' src='...' alt='Card image cap'>";
+          newHtml += '<div class="card-block">';
+          newHtml += '<h4 class="card-title">' + dataJson[i].name + '</h4>';
+          newHtml += '<p class="card-text">' + dataJson[i].description + '</p>';
+          newHtml += "<p class='card-text'>Found by: " + dataJson[i].found_by + "</p>";
+          newHtml += '<p class="card-text"><small class="text-muted">Date found: ' + dataJson[i].date_found + '</small></p>';
+          newHtml += '</div><div class="card-footer"><button class="btn btn-dos" type="button" name="button">Claim</button></div>'
+          newHtml += "</div>";
+        }
+        $("#objectsList").append(newHtml);
+      },
+      error: function(erroMsg) {
+        alert(erroMsg.statusText);
+      }
+    });
+  })
+
 
 });
